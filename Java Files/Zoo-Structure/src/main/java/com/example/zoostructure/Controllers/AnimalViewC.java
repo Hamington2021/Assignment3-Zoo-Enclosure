85d3785ae576d297faeab556b028dcc5750dad69
MrHarte <111158364+MrHarte@users.noreply.github.com>
2025-11-03 13:22:33 -0500

    Added Controllers and Model packages
    Added AnimalViewController to Controllers package
    Added Animal Class to Model package

---
+++ Java Files/Zoo-Structure/src/main/java/com/example/zoostructure/Controllers/AnimalViewC.java
@@ -0,0 +1,27 @@
+package com.example.zoostructure.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+public class AnimalViewC {
+    @FXML
+    private ChoiceBox<String> aTypeChoiceBox;
+
+    @FXML
+    private Spinner<Double> aAgeSpinner;
+
+    @FXML
+    private TextField aNameTextField;
+
+    @FXML
+    protected void onSaveButtonClick() {
+
+    }
+
+    @FXML
+    protected void onBackButtonClick() {
+
+    }
+}
