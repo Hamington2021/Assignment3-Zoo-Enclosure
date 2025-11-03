4006ca088a3b86531d320a3678b7696ca5269916
Serena <serena.shushack@stu.crc-lennox.qc.ca>
2025-11-03 14:02:25 -0500

    Working on setting up composite enclosure controller

---
+++ Java Files/Zoo-Structure/src/main/java/com/example/zoostructure/Controllers/CompositeEnclosureController.java
@@ -0,0 +1,25 @@
+package com.example.zoostructure.Controllers;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class CompositeEnclosureController {
+
+    @FXML
+    private Label sectionNameLabel;
+    @FXML
+    private Button newSectionButton;
+    @FXML
+    private Button lionViewButton;
+    @FXML
+    private Button tigerViewButton;
+    @FXML
+    private Button cougarViewButton;
+    @FXML
+    private MenuItem lionMenuItem;
+    @FXML
+    private MenuItem tigerMenuItem;
+    @FXML
+    private MenuItem cougarMenuItem;
+}
