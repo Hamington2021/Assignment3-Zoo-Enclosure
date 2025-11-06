8bd05a7ed2ea2648dec3cb4e7766cf74fcf08026
Serena <serena.shushack@stu.crc-lennox.qc.ca>
2025-11-06 16:30:17 -0500

    Working on basic composite enclosure structure. Implementing interface defined methods.

--- Java Files/Zoo-Structure/src/main/java/com/example/zoostructure/Model/CompositeEnclosureCollection.java
+++ Java Files/Zoo-Structure/src/main/java/com/example/zoostructure/Model/CompositeEnclosureCollection.java
@@ -1,5 +1,64 @@
 package com.example.zoostructure.Model;
 
+import java.util.List;
+
+/**
+ * CompositeEnclosureCollection represents a group of enclosures, which
+ * can be other composite enclosures or individual enclosures.\
+ * @author Serena
+ */
 public class CompositeEnclosureCollection implements EnclosureCollection {
 
+    /**
+     * The name of the enclosure.
+     */
+    private String aName;
+
+    /**
+     * The list of enclosures.
+     */
+    private List<EnclosureCollection> aEnclosures;
+
+    /**
+     * Returns the name of the enclosure.
+     * @return the name of this enclosure
+     */
+    @Override
+    public String getName() {
+        return aName;
+    }
+
+    /**
+     * Adds an enclosure to this collection.
+     * @param pEnclosure the enclosure to add
+     */
+    @Override
+    public void add(EnclosureCollection pEnclosure) {
+
+    }
+
+    /**
+     * Removes an enclosure from this collection.
+     * @param pEnclosure the enclosure to remove
+     */
+    @Override
+    public void remove(EnclosureCollection pEnclosure) {
+
+    }
+
+    /**
+     * Displays the enclosure.
+     */
+    @Override
+    public void display() {
+
+    }
+
+    /**
+     * Returns the list of enclosures.
+     * @return the list of enclosures
+     */
+    public List<EnclosureCollection> getEnclosures() {
+        return aEnclosures;
+    }
 }
